508f91df02bf4c4404531ea31e952641750d31e8
Gruzin <gruzd.roman@inbox.ru>
2023-03-14 22:40:20 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -3,4 +3,13 @@ from typing import Dict
 
 def get_dict(s: str) -> Dict[str, int]:
     # TODO: Add your code here
-    return {}
+    new_dict = {}
+    for i in s:
+        if i in new_dict:
+            new_dict[i] += 1
+        else:
+            new_dict[i] = 1
+    return new_dict
+
+
+
